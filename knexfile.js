573ba12183feab44f5bf733da73915cdec945595
valerialauanda <91025814+valerialauanda@users.noreply.github.com>
2021-12-09 14:31:59 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -10,16 +10,5 @@ module.exports = {
       directory: "./src/database/migrations"
     },
     useNullAsDefault: true
-  },
-  production: {
-    client: "pg",
-    connection: {
-      connectionStrings: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-      },
-      migrations : {
-        directory: "./src/database/migrations"
-    },
-
-  },
+  }
 };
